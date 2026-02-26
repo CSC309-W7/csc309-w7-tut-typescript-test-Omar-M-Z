bc568ea5dd0a610431c608c317ca3140188e8f0d
omarzaheer <omar.m.zaheer@gmail.com>
2026-02-25 20:04:26 -0500

    asdF

--- src/users.ts
+++ src/users.ts
@@ -2,14 +2,14 @@ import type { User } from "./types";
 
 export const apiResponse: unknown = [
   { name: "Tony", age: 23 },
-  { name: "Kevin", age: "24" }, // invalid
+  { name: "Kevin", age: 24 },
   { name: "Jim", age: 25 },
 ];
 
 export function getUsersData(): User[] {
-  return apiResponse as User[]; // intentionally unsafe
+    return <User[]>apiResponse;
 }
 
-export function formatAges(users: User[]): string[] {
-  return users.map((u) => u.age.toFixed(0));
+export function formatAges(users: User[]): Number[] {
+  return users.map((u) => u.age);
 }
